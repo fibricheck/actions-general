7640aad4018dc816e198c9ed5edb763d84905130
Estelle Berghmans <6025223+stel-b@users.noreply.github.com>
2026-04-29 15:37:42 +0200

    feat: updated xcode cloud sdk
    * Updated the appstore-connect-sdk to v2.0.0
    * Fixed issues with the xcode-cloud action

--- xcode-cloud/src/appstore-sdk.test.ts
+++ xcode-cloud/src/appstore-sdk.test.ts
@@ -1,6 +1,7 @@
 import "@dotenvx/dotenvx/config";
 import { describe, test, expect } from "@jest/globals";
 import { AppStoreApi, assertRepositoryId } from "./appstore-sdk.js";
+import { cancelXCodeCloudBuild, startXCodeCloudBuild } from "./core.js";
 
 const api = new AppStoreApi({
   issuerId: process.env.ISSUER_ID,
@@ -8,7 +9,7 @@ const api = new AppStoreApi({
   privateKey: process.env.PRIVATE_KEY,
 });
 
-const bundleId = process.env.BUNDLE_ID;
+const bundleId = process.env.BUNDLE_ID!!;
 
 describe("api test", () => {
   test("can retrieve repository and product", async () => {
@@ -26,31 +27,53 @@ describe("api test", () => {
 
   test("can retrieve git reference", async () => {
     const gitReference = await api.getGitReference(
-      process.env.REPOSITORY_ID,
-      process.env.GIT_REF
+      process.env.REPOSITORY_ID!!,
+      process.env.GIT_REF!!
     );
     expect(gitReference.id).toBeDefined();
-  });
+  }, 20000);
 
   test("can paginate through all git references", async () => {
     await expect(api.getGitReference(
-      process.env.REPOSITORY_ID,
+      process.env.REPOSITORY_ID!!,
       'non existent'
     )).rejects.toThrow();
-  });
+  }, 20_000);
 
   test("can retrieve workflow", async () => {
     const workflow = await api.getWorkflowByName(
-      process.env.PRODUCT_ID,
-      process.env.WORKFLOW_NAME
+      process.env.PRODUCT_ID!!,
+      process.env.WORKFLOW_NAME!!
     );
     expect(workflow.id).toBeDefined();
   });
 
   test("can paginate through all workflows", async () => {
     await expect(api.getWorkflowByName(
-      process.env.PRODUCT_ID,
+      process.env.PRODUCT_ID!!,
       'non existent'
     )).rejects.toThrow()
   });
+
+  // test("can build and cancel", async () => {
+  //   const buildResult = await startXCodeCloudBuild({
+  //     issuerId: process.env.ISSUER_ID!!,
+  //     bundleId: bundleId!!,
+  //     privateKeyId: process.env.PRIVATE_KEY_ID!!,
+  //     privateKey: process.env.PRIVATE_KEY!!,
+  //     workflowName: process.env.WORKFLOW_NAME!!,
+  //     gitRef: process.env.GIT_REF!!,
+  //   })
+
+  //   expect(buildResult.buildId).toBeDefined();
+
+  //   const cancelResult = await cancelXCodeCloudBuild({
+  //     issuerId: process.env.ISSUER_ID!!,
+  //     privateKeyId: process.env.PRIVATE_KEY_ID!!,
+  //     privateKey: process.env.PRIVATE_KEY!!,
+  //     buildId: buildResult.buildId!
+  //   })
+
+  //   expect(cancelResult).toBeDefined();
+  // }, 20000)
 });
